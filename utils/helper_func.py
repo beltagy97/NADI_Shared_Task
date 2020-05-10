69b4514cfaa2b85396de895732ac58cfeccfe08c
abdelrahman wael <abdel.rahman.wael1@gmail.com>
2020-05-10 02:25:17 +0200

    added function to return prediction from logits

--- utils/helper_func.py
+++ utils/helper_func.py
@@ -2,11 +2,19 @@ import numpy as np
 import time
 import datetime
 
-def flat_accuracy(preds, labels,device):
-  pred_flat = np.argmax(preds.cpu().detach().numpy(), axis=1).flatten()
+def flat_accuracy(logits, labels,device):
+  pred_flat = np.argmax(logits.cpu().detach().numpy(), axis=1).flatten()
   labels_flat = labels.cpu().detach().numpy().flatten()
   return np.sum(pred_flat == labels_flat) / len(labels_flat)
-    
+
+def get_preds(logits,device):
+  preds = np.argmax(logits.cpu().detach().numpy(), axis=1).flatten()
+  return preds.tolist()
+
+def get_labels(labels,device):
+  labels = labels.cpu().detach().numpy().flatten()
+  return labels.tolist()
+
 def format_time(elapsed):
     '''
     Takes a time in seconds and returns a string hh:mm:ss
